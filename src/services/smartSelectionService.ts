757c333667f02714628a958e67d8d95a5888cd22
Stepan Burguchev <skara06@gmail.com>
2018-03-14 16:31:06 +0300

    Smart selection: string literals; brackets; siblings

--- src/services/smartSelectionService.ts
+++ src/services/smartSelectionService.ts
@@ -1,6 +1,41 @@
-import { Node, Printable, StringLiteral, TypeName } from 'ast-types';
+import { Node, NodePath, Printable, StringLiteral, TypeName } from 'ast-types';
 import * as vscode from 'vscode';
 import astService, { AstRoot, LanguageId, Selection } from './astService';
+
+interface UnordedSelection {
+    start: number;
+    end: number;
+}
+
+function fromSelection(selection: Selection) {
+    let start;
+    let end;
+    if (selection.anchor > selection.active) {
+        start = selection.active;
+        end = selection.anchor;
+    } else {
+        start = selection.anchor;
+        end = selection.active;
+    }
+    return {
+        start,
+        end
+    };
+}
+
+function toSelection(result: { start: number; end: number }, previousSelection: Selection) {
+    if (previousSelection.anchor > previousSelection.active) {
+        return {
+            anchor: result.end,
+            active: result.start
+        };
+    } else {
+        return {
+            anchor: result.start,
+            active: result.end
+        };
+    }
+}
 
 class SmartSelectionService {
     public extendSelection(languageId: LanguageId, ast: AstRoot, selection: Selection): Selection {
@@ -10,89 +45,121 @@ class SmartSelectionService {
             return selection;
         }
 
-        let start;
-        let end;
-        if (selection.anchor > selection.active) {
-            start = selection.active;
-            end = selection.anchor;
-        } else {
-            start = selection.anchor;
-            end = selection.active;
+        const { start, end } = fromSelection(selection);
+        let result = {
+            start,
+            end
+        };
+        let targetNode = target.firstNode<Node>()!;
+        let targetPath = target.firstPath<Node>()!;
+        const completeCoverage = start === targetNode.start && end === targetNode.end;
+        if (completeCoverage) {
+            if (!targetPath.parentPath) {
+                // root object achieved -> no change
+                return selection;
+            }
+
+            if (
+                Array.isArray(targetPath.parentPath.value) &&
+                targetPath.parentPath.value.length > 1
+            ) {
+                // extend to siblings -> return result
+                const siblings = targetPath.parentPath.value as Node[];
+                result.start = siblings[0].start;
+                result.end = siblings[siblings.length - 1].end;
+                return toSelection(result, selection);
+            }
+
+            // switch node to parent -> proceed as planned
+            targetPath = targetPath.parent as NodePath<Node>;
+            targetNode = targetPath.node;
         }
 
-        const targetNode = target.firstNode<Node>()!;
-        const targetPath = target.firstPath<Node>()!;
-        const incompleteCoverage = start > targetNode.start || end < targetNode.end;
-        if (incompleteCoverage) {
-            switch (targetNode.type as TypeName) {
-                case 'StringLiteral':
-                    if (targetNode.start === start || targetNode.end === end) {
-                        // 'cont|ent'| => |'content'|
-                        start = targetNode.start;
-                        end = targetNode.end;
-                    } else if (targetNode.start + 1 === start && targetNode.end - 1 === end) {
-                        // '|content|' => |'content'|
-                        start = targetNode.start;
-                        end = targetNode.end;
-                    } else {
-                        const value = (targetNode as StringLiteral).value;
-
-                        let startIndex = start - targetNode.start - 1;
-                        let endIndex = end - targetNode.start - 1;
-                        let expanded = false;
-                        while (startIndex > 0 && /[a-zA-Z0-9$_]/.test(value[startIndex - 1])) {
-                            startIndex--;
-                            expanded = true;
-                        }
-                        while (endIndex < value.length && /[a-zA-Z0-9$_]/.test(value[endIndex])) {
-                            endIndex++;
-                            expanded = true;
-                        }
-
-                        if (expanded) {
-                            // 'content is a se|nt|ence' => 'content is a |sentence|'
-                            start = startIndex + targetNode.start + 1;
-                            end = endIndex + targetNode.start + 1;
-                        } else {
-                            // 'content is a |sentence|' => '|content is a sentence|'
-                            start = targetNode.start + 1;
-                            end = targetNode.end - 1;
-                        }
-                    }
-                    break;
-                default:
-                    start = targetNode.start;
-                    end = targetNode.end;
-                    break;
-            }
-        } else if (j.Node.check(targetPath.parent.node)) {
-            switch (targetPath.parent.node.type) {
-                default:
-                    start = targetPath.parent.node.start;
-                    end = targetPath.parent.node.end;
-                    break;
-            }
-        } else {
+        if (!j.Node.check(targetNode)) {
             return selection;
         }
 
-        if (selection.anchor > selection.active) {
-            return {
-                anchor: end,
-                active: start
-            };
-        } else {
-            return {
-                anchor: start,
-                active: end
-            };
+        switch (targetNode.type as TypeName) {
+            case 'StringLiteral':
+                result = this._extendStringLiteral(targetNode as StringLiteral, start, end);
+                break;
+            case 'BlockStatement':
+            case 'ArrayExpression':
+            case 'ObjectExpression':
+                result = this._extendBracketNode(targetNode, start, end);
+                break;
+            default:
+                result.start = targetNode.start;
+                result.end = targetNode.end;
+                break;
         }
+
+        return toSelection(result, selection);
     }
 
     public shrinkSelection(languageId: LanguageId, ast: AstRoot, selection: Selection): Selection {
         const jscodeshift = astService.getCodeShift(languageId);
         const target = ast.findNodeInRange(selection.anchor, selection.active);
         return selection;
+    }
+
+    private _extendBracketNode(targetNode: Node, start: number, end: number): UnordedSelection {
+        if (targetNode.start === start || targetNode.end === end) {
+            // { expres|sions }| => |{ expressions }|
+            start = targetNode.start;
+            end = targetNode.end;
+        } else if (targetNode.start + 1 === start && targetNode.end - 1 === end) {
+            // {| expressions |} => |{ expressions }|
+            start = targetNode.start;
+            end = targetNode.end;
+        } else {
+            // { ex|press|ions } => {| expressions |}
+            start = targetNode.start + 1;
+            end = targetNode.end - 1;
+        }
+
+        return { start, end };
+    }
+
+    private _extendStringLiteral(
+        targetNode: StringLiteral,
+        start: number,
+        end: number
+    ): UnordedSelection {
+        if (targetNode.start === start || targetNode.end === end) {
+            // 'cont|ent'| => |'content'|
+            start = targetNode.start;
+            end = targetNode.end;
+        } else if (targetNode.start + 1 === start && targetNode.end - 1 === end) {
+            // '|content|' => |'content'|
+            start = targetNode.start;
+            end = targetNode.end;
+        } else {
+            const value = targetNode.value;
+            let startIndex = start - targetNode.start - 1;
+            let endIndex = end - targetNode.start - 1;
+            let expanded = false;
+            while (startIndex > 0 && /[a-zA-Z0-9$_]/.test(value[startIndex - 1])) {
+                startIndex--;
+                expanded = true;
+            }
+            while (endIndex < value.length && /[a-zA-Z0-9$_]/.test(value[endIndex])) {
+                endIndex++;
+                expanded = true;
+            }
+
+            if (expanded) {
+                // 'content is a se|nt|ence' => 'content is a |sentence|'
+                start = startIndex + targetNode.start + 1;
+                end = endIndex + targetNode.start + 1;
+            } else {
+                // 'content is a |sentence|' => '|content is a sentence|'
+                start = targetNode.start + 1;
+                end = targetNode.end - 1;
+            }
+        }
+
+        return { start, end };
     }
 }
 
--- src/test/smartSelection/smartSelection.test.ts
+++ src/test/smartSelection/smartSelection.test.ts
@@ -1,16 +1,6 @@
 import { assertSmartSelectionExtend } from '../utils/smartSelectionHelpers';
 
 suite(`Smart selection: extend`, () => {
-    test('should extend to Identifier', () => {
-        const before = `
-            let fo|ob|ar;
-        `;
-        const after = `
-            let |foobar|;
-        `;
-        assertSmartSelectionExtend(before, after);
-    });
-
     test('should extend to word', () => {
         const before = `
             let a = 'content is a sen|te|nce';
@@ -47,6 +37,130 @@ suite(`Smart selection: extend`, () => {
         `;
         const after = `
             let a = |'content is a sentence'|;
+        `;
+        assertSmartSelectionExtend(before, after);
+    });
+
+    test('should extend to whole node: Identifier', () => {
+        const before = `
+            let fo|ob|ar;
+        `;
+        const after = `
+            let |foobar|;
+        `;
+        assertSmartSelectionExtend(before, after);
+    });
+
+    test('should extend to brackets within node: [ ]', () => {
+        const before = `
+            let array = [
+                |'foo',
+                'bar'|
+            ];
+        `;
+        const after = `
+            let array = [|
+                'foo',
+                'bar'
+            |];
+        `;
+        assertSmartSelectionExtend(before, after);
+    });
+
+    test('should extend to whole node: [ ]', () => {
+        const before = `
+            let array = [|
+                'foo',
+                'bar'
+            |];
+        `;
+        const after = `
+            let array = |[
+                'foo',
+                'bar'
+            ]|;
+        `;
+        assertSmartSelectionExtend(before, after);
+    });
+
+    test('should extend to whole node 2: [ ]', () => {
+        const before = `
+            let array = |[
+                'foo',
+                'ba|r'
+            ];
+        `;
+        const after = `
+            let array = |[
+                'foo',
+                'bar'
+            ]|;
+        `;
+        assertSmartSelectionExtend(before, after);
+    });
+
+    test('should extend to brackets within node: {} if', () => {
+        const before = `
+            if (true) {
+                |let a;
+                let b;|
+            }
+        `;
+        const after = `
+            if (true) {|
+                let a;
+                let b;
+            |}
+        `;
+        assertSmartSelectionExtend(before, after);
+    });
+
+    test('should extend to brackets within node: {} object', () => {
+        const before = `
+            let obj = {
+                |foo: 'foo',
+                bar: 'bar'|
+            };
+        `;
+        const after = `
+            let obj = {|
+                foo: 'foo',
+                bar: 'bar'
+            |};
+        `;
+        assertSmartSelectionExtend(before, after);
+    });
+
+    test('should extend to siblings: object properties', () => {
+        const before = `
+            let obj = {
+                |foo: 'foo'|,
+                bar: 'bar'
+            };
+        `;
+        const after = `
+            let obj = {
+                |foo: 'foo',
+                bar: 'bar'|
+            };
+        `;
+        assertSmartSelectionExtend(before, after);
+    });
+
+    test('should extend to siblings: statements', () => {
+        const before = `
+            function test() {
+                let a;
+                |let b;|
+                let c;
+            }
+        `;
+        const after = `
+            function test() {
+                |let a;
+                let b;
+                let c;|
+            }
         `;
         assertSmartSelectionExtend(before, after);
     });
