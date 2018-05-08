1a8be0ea16fe57dd912c37e78388126da6554cc4
Stepan Burguchev <skara06@gmail.com>
2018-05-08 23:53:29 +0300

    Ignore generated code

--- client/server/src/models/CodeMod.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CodeModScope;
-(function (CodeModScope) {
-    CodeModScope["Global"] = "global";
-    CodeModScope["Cursor"] = "cursor";
-})(CodeModScope = exports.CodeModScope || (exports.CodeModScope = {}));
-//# sourceMappingURL=CodeMod.js.map
